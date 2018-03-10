e7b5b21a60e42d6278dc312b6da314e3301e623e
[zhangqing] <[58402255@qq.com]>
2018-03-10 18:35:02 +0800

    tags

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -296,6 +296,8 @@
           console.log(arguments);
 
           return item.ResearchOrganization.toString().includes(val);
+          //if(item.title.toString().includes(val)) return true;
+          //if(item.body.toString().includes(val)) return true;
         });
       }
     },
